2621e20373cb43550e6446986144cc8d3912b950
Juan Carmona <jcarmonau@insail.com>
2022-06-09 12:28:50 +0200

    Add new services

--- src/types.d.ts
+++ src/types.d.ts
@@ -2,9 +2,13 @@ export type Weather = 'sunny' | 'rainy' | 'cloudy' | 'windy' | 'stormy'
 export type Visibility = 'great' | 'good' | 'ok' | 'poor'
 
 export interface DiaryEntry {
-  id: number
+  id: number | string
   date: string
   weather: Weather
   visibility: Visibility
   comment: string
 }
+
+// export type NonSensitiveInfoDiaryEntry = Pick<DiaryEntry, 'id' | 'date' | 'weather' | 'visibility'>
+export type NonSensitiveInfoDiaryEntry = Omit<DiaryEntry, 'comment'>
+export type newDiaryEntry = Omit<DiaryEntry, 'id'>
